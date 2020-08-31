d50d813d4ba635871cea864765abea8a82eba8b2
Sienna Kopf <princess.kopf@gmail.com>
2020-08-31 15:36:31 -0600

    Add controllers for items and merchants

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    render json: ItemSerializer.new(Item.all)
+  end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
