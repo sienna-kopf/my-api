bb642e6f075888fb6668a16cd48b389fdeb57fdd
Sienna Kopf <princess.kopf@gmail.com>
2020-08-31 16:28:07 -0600

    Update spec to not check for a given response

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -112,8 +112,5 @@ RSpec.describe "Items API ", type: :request do
 
     expect(Item.count).to eq(0)
     expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
-
-    deleted_item = JSON.parse(response.body, symbolize_names: true)
-    expect(deleted_item[:data]).to eq([])
   end
 end
