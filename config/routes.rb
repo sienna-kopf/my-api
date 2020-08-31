6097cee4ba19df81eeccfc19049353b2ac5a2da6
Sienna Kopf <princess.kopf@gmail.com>
2020-08-31 14:47:35 -0600

    Change to use resources for namespaced routes items and merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
