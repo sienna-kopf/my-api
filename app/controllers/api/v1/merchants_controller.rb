0a1eae9e240cd3b53de2c5dacf5f7f972b6534a7
Sienna Kopf <princess.kopf@gmail.com>
2020-08-31 21:56:11 -0600

    Add to controller update action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -12,6 +12,7 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def update
+    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
   end
 
   def destroy
