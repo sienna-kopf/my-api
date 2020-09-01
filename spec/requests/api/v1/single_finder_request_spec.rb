98329fa9309cfc39f7ee6cb06784754a028719e8
Sienna Kopf <princess.kopf@gmail.com>
2020-09-01 15:36:50 -0600

    Write tests for single finders for items

---
+++ spec/requests/api/v1/single_finder_request_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe "Single Finders", type: :request do
+  it "can retrieve a single item based on an attribute search part of word" do
+    merchant = create(:merchant)
+    item_1 = create(:item, merchant: merchant)
+    item_2 = Item.create!(name: "Quickdraws", description: "Technical safety eq.", unit_price: 45.50, merchant: merchant)
+    item_3 = Item.create!(name: "Drawsaurus Stuffed Animal", description: "Based off the hit pictionary game", unit_price: 5.75, merchant: merchant)
+
+    get "/api/v1/items/find?name=quick"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data]).to have_key(:type)
+    expect(item[:data]).to have_key(:attributes)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+  end
+
+  it "can retrieve a single item based on an attribute search full word" do
+    merchant = create(:merchant)
+    item_1 = create(:item, merchant: merchant)
+    item_2 = Item.create!(name: "Quickdraws", description: "Technical safety eq.", unit_price: 45.50, merchant: merchant)
+    item_3 = Item.create!(name: "Drawsaurus Stuffed Dinosaur", description: "Based off the hit pictionary game", unit_price: 5.75, merchant: merchant)
+
+    get "/api/v1/items/find?name=Dinosaur"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data]).to have_key(:type)
+    expect(item[:data]).to have_key(:attributes)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+  end
+end
