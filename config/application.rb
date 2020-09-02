edb1ef7c4091306db2ae3e9eea5fd167541823d1
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 10:04:48 -0600

    Add configuration to handle coverage pushes

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module MyApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    initializer(:remove_activestorage_routes, after: :add_routing_paths) {|app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}}
   end
 end
