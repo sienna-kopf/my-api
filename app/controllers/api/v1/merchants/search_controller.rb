66b5201b47d35646b5d8009737adfea629a7f8d8
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 12:18:29 -0600

    limit merchant results to 1 and change action name back to show

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::Merchants::SearchController < ApplicationController
-  def index
+  def show
     attribute = params.keys.first
     keyword = params[attribute]
     if attribute == "name"
-      merchant = Merchant.where("merchants.#{attribute} ILIKE ?", "%#{keyword}%")
+      merchant = Merchant.where("merchants.#{attribute} ILIKE ?", "%#{keyword}%").limit(1)
     elsif attribute == "created_at" || attribute == "updated_at"
       keyword = keyword.to_s
-      merchant = Merchant.where("DATE(#{attribute}) = ?", "#{keyword}")
+      merchant = Merchant.where("DATE(#{attribute}) = ?", "#{keyword}").limit(1)
     else
-      merchant = Merchant.where("#{attribute} = ?", "#{keyword}")
+      merchant = Merchant.where("#{attribute} = ?", "#{keyword}").limit(1)
     end
     render json: MerchantSerializer.new(merchant)
   end
