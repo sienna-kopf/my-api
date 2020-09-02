9fe3c0118906124c46a8bb02dadbd3edb20c91da
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 13:44:34 -0600

    Replace where limit(1) with a find_by to make the processed json payload in the correct format for the spec harness

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::Items::SearchController < ApplicationController
     attribute = params.keys.first
     keyword = params[attribute]
     if attribute == "name" || attribute == "description"
-      item = Item.where("items.#{attribute} ILIKE ?", "%#{keyword}%").limit(1)
+      item = Item.find_by("items.#{attribute} ILIKE ?", "%#{keyword}%")
     elsif attribute == "created_at" || attribute == "updated_at"
       keyword = keyword.to_s
-      item = Item.where("DATE(#{attribute}) = ?", "#{keyword}").limit(1)
+      item = Item.find_by("DATE(#{attribute}) = ?", "#{keyword}")
     else
-      item = Item.where("#{attribute} = ?", "#{keyword}").limit(1)
+      item = Item.find_by("#{attribute} = ?", "#{keyword}")
     end
     render json: ItemSerializer.new(item)
   end
--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::Merchants::SearchController < ApplicationController
     attribute = params.keys.first
     keyword = params[attribute]
     if attribute == "name"
-      merchant = Merchant.where("merchants.#{attribute} ILIKE ?", "%#{keyword}%").limit(1)
+      merchant = Merchant.find_by("merchants.#{attribute} ILIKE ?", "%#{keyword}%")
     elsif attribute == "created_at" || attribute == "updated_at"
       keyword = keyword.to_s
-      merchant = Merchant.where("DATE(#{attribute}) = ?", "#{keyword}").limit(1)
+      merchant = Merchant.find_by("DATE(#{attribute}) = ?", "#{keyword}")
     else
-      merchant = Merchant.where("#{attribute} = ?", "#{keyword}").limit(1)
+      merchant = Merchant.find_by("#{attribute} = ?", "#{keyword}")
     end
     render json: MerchantSerializer.new(merchant)
   end
