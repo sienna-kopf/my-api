9281711c28a2756748d3e6a3822d2f73dd795709
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 10:16:46 -0600

    Add merchants search controller with show method that contains conditional logic based on attribute type to make a AR Call-- DOES NOT WORK For dates

---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::Merchants::SearchController < ApplicationController
+  def show
+    attribute = params.keys.first
+    keyword = params[attribute]
+    if attribute == "name"
+      merchant = Merchant.where("merchants.#{attribute} ILIKE ?", "%#{keyword}%")
+    elsif attribute == "created_at" || attribute == "updated_at"
+      merchant = Merchant.where("DATE(#{attribute}) LIKE ?", "%#{keyword}%")
+    else
+      merchant = Merchant.where("#{attribute} = ?", "#{keyword}")
+    end
+    render json: MerchantSerializer.new(merchant)
+  end
+end
