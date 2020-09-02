16dfa59cc94f7e4bc1f4ca7f175578d198eba5a3
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 12:39:12 -0600

    Create new index action for merchants search controller and repeat much of the same code to find the correct merchant data for serialization

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -12,4 +12,18 @@ class Api::V1::Merchants::SearchController < ApplicationController
     end
     render json: MerchantSerializer.new(merchant)
   end
+
+  def index
+    attribute = params.keys.first
+    keyword = params[attribute]
+    if attribute == "name"
+      merchant = Merchant.where("merchants.#{attribute} ILIKE ?", "%#{keyword}%")
+    elsif attribute == "created_at" || attribute == "updated_at"
+      keyword = keyword.to_s
+      merchant = Merchant.where("DATE(#{attribute}) = ?", "#{keyword}")
+    else
+      merchant = Merchant.where("#{attribute} = ?", "#{keyword}")
+    end
+    render json: MerchantSerializer.new(merchant)
+  end
 end
