b60c81c611956f6a9c881571a642b8ccee2221c2
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 10:04:27 -0600

    Add namespaced route under items for a search controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :items do
+        get "/find", to: "search#show"
+      end
       resources :items, only: [:index, :show, :create, :update, :destroy]
-      resources :merchants, only: [:index, :show, :create, :update, :destroy]
-
-      get "/merchants/:id/items", to: "merchants#items"
       get "/items/:id/merchant", to: "items#merchant"
 
-      get "/items/find", to: "items#find"
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+      get "/merchants/:id/items", to: "merchants#items"
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
