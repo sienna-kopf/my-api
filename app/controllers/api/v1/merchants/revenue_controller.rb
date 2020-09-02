dbdcaeec45e34be89c0d23c5b64943333c22d215
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 15:33:46 -0600

    Call revenue facade from controller to send to revenue serializer

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
     merchant = Merchant.find(params[:id])
-    revenue = merchant.invoices.joins(:invoice_items).joins(:payments).select("sum(invoice_items.quantity * invoice_items.unit_price) as revenue").where("payments.result = 'success' AND status = 'shipped'").group(:id).order("revenue desc").first
-
-    render json: RevenueSerializer.new(revenue)
+    
+    render json: RevenueSerializer.new(RevenueFacade.merchant_revenue(merchant))
   end
 end
