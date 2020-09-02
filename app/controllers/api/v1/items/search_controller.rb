8df090bb4ce89c409c2a08ac25f15c7bde15946a
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 12:29:54 -0600

    Add index method to items controller that handles multi finder endpoint, very similar to single finder endpoint

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -12,4 +12,18 @@ class Api::V1::Items::SearchController < ApplicationController
     end
     render json: ItemSerializer.new(item)
   end
+
+  def index
+    attribute = params.keys.first
+    keyword = params[attribute]
+    if attribute == "name" || attribute == "description"
+      item = Item.where("items.#{attribute} ILIKE ?", "%#{keyword}%")
+    elsif attribute == "created_at" || attribute == "updated_at"
+      keyword = keyword.to_s
+      item = Item.where("DATE(#{attribute}) = ?", "#{keyword}")
+    else
+      item = Item.where("#{attribute} = ?", "#{keyword}")
+    end
+    render json: ItemSerializer.new(item)
+  end
 end
