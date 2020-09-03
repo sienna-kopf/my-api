6b70969a114790f99c13a8b3a05c345c6498e0c4
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 20:34:37 -0600

    Add merchants by items class method to new item facade to make AR query

---
+++ app/facades/item_facade.rb
@@ -0,0 +1,5 @@
+class ItemFacade
+  def self.merchants_by_items(quantity)
+    Merchant.joins(invoices: [:invoice_items, :payments]).select("merchants.*, sum(invoice_items.quantity) as items_sold").where("payments.result='success' AND invoices.status='shipped'").group(:id).order("items_sold desc").limit(quantity)
+  end
+end
