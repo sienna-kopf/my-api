3e4aa738345c736cf0fe1bd65208c5ec5ae3f22e
Sienna Kopf <princess.kopf@gmail.com>
2020-09-02 20:34:12 -0600

    Add most items action to merchant namespaced items controller

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::Merchants::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.where(merchant_id: params[:id]))
   end
+
+  def most_items
+    quantity = params[:quantity]
+
+    render json: MerchantSerializer.new(ItemFacade.merchants_by_items(quantity))
+  end
 end
